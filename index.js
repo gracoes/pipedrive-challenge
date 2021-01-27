5d01f7822d2f684b5304f01e66ba0aa889be832d
gracoes <goncalo.racoes@uniplaces.com>
2021-01-27 11:46:00 +0000

    Move dependencies injection to index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+import { open } from "sqlite";
+import sqlite3 from "sqlite3";
+
+import Repository from "./src/OrganizationsBundle/repository/index.js";
+import SqlAdapter from "./src/OrganizationsBundle/sql-adapters/sqlite/index.js";
 import app from "./src/app.js";
 
-(function () {
-  app();
+(async function () {
+  const db = await open({
+    filename: ":memory:",
+    driver: sqlite3.Database,
+  });
+
+  const adapter = await SqlAdapter(db);
+  const repository = Repository(adapter);
+
+  app({ repository });
 })();
