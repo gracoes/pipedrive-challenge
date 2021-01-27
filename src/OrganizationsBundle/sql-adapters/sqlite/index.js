dc09c0195296e596a4506aebcbac80fc6318903a
gracoes <goncalo.racoes@uniplaces.com>
2021-01-27 11:48:44 +0000

    Create fetch by name function in sqlite-adapter

--- src/OrganizationsBundle/sql-adapters/sqlite/index.js
+++ src/OrganizationsBundle/sql-adapters/sqlite/index.js
@@ -1,26 +1,45 @@
-export default function (dbClient) {
+export default async function Adapter(dbClient) {
+  await dbClient.exec(
+    "CREATE TABLE IF NOT EXISTS relations (head TEXT, tail TEXT, type TEXT, UNIQUE (head,tail,type))"
+  );
+
+  await dbClient.exec(
+    "CREATE INDEX IF NOT EXISTS head_tail on relations (head, tail)"
+  );
+
+  async function batchInsert(records) {
+    try {
+      const statements = await Promise.all(
+        records.map((row) => prepareInsert(row))
+      );
+
+      await dbClient.exec("BEGIN TRANSACTION");
+      await Promise.all(
+        statements.map((stmt) => stmt.run().then(({ stmt }) => stmt.finalize()))
+      );
+      await dbClient.exec("COMMIT TRANSACTION");
+
+      return true;
+    } catch (err) {
+      await dbClient.exec("ROLLBACK TRANSACTION");
+
+      throw new Error(err.message);
+    }
+  }
+
+  function prepareInsert({ head, tail, type }) {
+    return dbClient.prepare(
+      "INSERT INTO relations (head, tail, type) VALUES (?, ?, ?)",
+      [head, tail, type]
+    );
+  }
+
+  function fetchAllByName(name) {
+    return dbClient.all("SELECT * FROM relations WHERE head = :name", [name]);
+  }
+
   return {
-    batchInsert: (rows) => _batchInsert(dbClient, rows),
+    batchInsert,
+    fetchAllByName,
   };
-}
-
-async function _batchInsert(client, rows) {
-  const statements = await Promise.all(
-    rows.map((row) => prepareInsert(client, row))
-  );
-
-  await client.exec("BEGIN TRANSACTION");
-  await Promise.all(
-    statements.map((stmt) => stmt.run().then(({ stmt }) => stmt.finalize()))
-  );
-  await client.exec("COMMIT TRANSACTION");
-
-  return true;
-}
-
-function prepareInsert(client, { head, tail, type }) {
-  return client.prepare(
-    "INSERT INTO relations (head, tail, type) VALUES (?, ?, ?)",
-    [head, tail, type]
-  );
 }
--- src/OrganizationsBundle/sql-adapters/sqlite/index.test.js
+++ src/OrganizationsBundle/sql-adapters/sqlite/index.test.js
@@ -1,35 +1,101 @@
-import Adapter from "./index.js";
+import { jest } from "@jest/globals";
 import { open } from "sqlite";
 import sqlite3 from "sqlite3";
 
-let db;
-beforeAll(async () => {
-  db = await open({
-    filename: ":memory:",
-    driver: sqlite3.Database,
+import Adapter from "./index.js";
+
+describe("SQLite Adapter", () => {
+  let db;
+  beforeAll(async () => {
+    db = await open({
+      filename: ":memory:",
+      driver: sqlite3.Database,
+    });
   });
 
-  await db.exec("CREATE TABLE relations (head TEXT, tail TEXT, type TEXT)");
-});
+  afterAll(async () => {
+    await db.exec("DROP TABLE relations");
+    await db.close();
+  });
 
-afterAll(async () => {
-  await db.exec("DROP TABLE relations");
-  await db.close();
-});
+  afterEach(async () => {
+    await db.exec("DELETE FROM relations");
+  });
 
-beforeEach(async () => {
-  await db.exec("DELETE FROM relations");
-});
+  test("it creates relations table when initiated", async () => {
+    await Adapter(db);
 
-test("batch insert", async () => {
-  const adapter = Adapter(db);
+    const records = await db.all("SELECT * FROM relations");
 
-  const res = await adapter.batchInsert([
-    { head: "Parent", tail: "Child 1", type: "parent" },
-  ]);
+    expect(records).toEqual([]);
+  });
 
-  const row = await db.get("SELECT * FROM relations");
+  test("it creates index on relations table when initiated", async () => {
+    await Adapter(db);
 
-  expect(res).toBeTruthy();
-  expect(row).toEqual({ head: "Parent", tail: "Child 1", type: "parent" });
+    const records = await db.all(
+      "SELECT * FROM sqlite_master WHERE type = 'index' AND name = 'head_tail'"
+    );
+
+    expect(records).toHaveLength(1);
+  });
+
+  test("it inserts multiple records", async () => {
+    const adapter = await Adapter(db);
+
+    const res = await adapter.batchInsert([
+      { head: "Parent", tail: "Child 1", type: "parent" },
+    ]);
+
+    const row = await db.get("SELECT * FROM relations");
+
+    expect(res).toBeTruthy();
+    expect(row).toEqual({ head: "Parent", tail: "Child 1", type: "parent" });
+  });
+
+  test("inserting multiple records is atomic", async () => {
+    const mockDb = {
+      exec: jest.fn(() => Promise.resolve()),
+      prepare: jest
+        .fn()
+        .mockImplementationOnce((statement, params) =>
+          db.prepare(statement, params)
+        )
+        .mockImplementationOnce(() =>
+          Promise.reject({ message: "Something went wrong!" })
+        ),
+    };
+    const adapter = await Adapter(mockDb);
+    const insertMany = () =>
+      adapter.batchInsert([
+        { head: "Parent", tail: "Child 1", type: "parent" },
+        { head: "Parent", tail: "Child 2", type: "parent" },
+      ]);
+
+    await expect(insertMany).rejects.toThrow("Something went wrong!");
+
+    const records = await db.all("SELECT * FROM relations");
+
+    expect(records).toHaveLength(0);
+  });
+
+  test("it fetches records by name ", async () => {
+    const adapter = await Adapter(db);
+
+    await db.exec(
+      "INSERT INTO relations VALUES ('Parent', 'Child 2', 'parent')"
+    );
+    await db.exec(
+      "INSERT INTO relations VALUES ('Parent', 'Child 1', 'parent')"
+    );
+
+    const rows = await adapter.fetchAllByName("Parent");
+
+    expect(rows).toEqual(
+      expect.arrayContaining([
+        { head: "Parent", tail: "Child 1", type: "parent" },
+        { head: "Parent", tail: "Child 2", type: "parent" },
+      ])
+    );
+  });
 });
