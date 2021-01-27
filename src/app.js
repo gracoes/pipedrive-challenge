69faa11c1be9d6ee672218b08f8aaa668bb36021
gracoes <goncalo.racoes@uniplaces.com>
2021-01-27 11:46:50 +0000

    Use getHandler to serve get-relations endpoint

--- src/app.js
+++ src/app.js
@@ -3,14 +3,14 @@ import express from "express";
 import createHandler from "./OrganizationsBundle/handlers/create/index.js";
 import getHandler from "./OrganizationsBundle/handlers/get/index.js";
 
-export default function () {
+export default function ({ repository }) {
   const server = express();
 
   server.use(express.json());
 
   server.get("/ping", (_, res) => res.json({ message: "Hello World" }, 200));
-  server.post("/organization/relations", createHandler());
-  server.get("/organization/:name/relations", getHandler);
+  server.post("/organization/relations", createHandler(repository));
+  server.get("/organization/:name/relations", getHandler(repository));
 
   server.listen(8080, () => {
     console.log("Listening on port 8080");
