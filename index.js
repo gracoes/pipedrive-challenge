67db48e3034bdc10bdc40e2bba903d467fcbe8a0
gracoes <goncalo.racoes@uniplaces.com>
2021-01-28 15:25:12 +0000

    Fix import

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import sqlite3 from "sqlite3";
 
 import app from "./src/app.js";
 import Repository from "./src/OrganizationsBundle/repository/index.js";
-import SqlAdapter from "./src/OrganizationsBundle/sql-adapters/sqlite/index.js";
+import SqlAdapter from "./src/OrganizationsBundle/adapters/sqlite/index.js";
 
 (async function () {
   const db = await open({
