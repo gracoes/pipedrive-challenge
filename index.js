6b221dd18d40b29df77317a42fab0ccbfee66960
gracoes <goncalo.racoes@uniplaces.com>
2021-01-29 15:39:11 +0000

    Add graceful shutdown

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { open } from "sqlite";
 import sqlite3 from "sqlite3";
+import http from "http";
+import { createTerminus } from "@godaddy/terminus";
 
 import app from "./src/app.js";
 import Repository from "./src/OrganizationsBundle/repository/index.js";
@@ -14,7 +16,24 @@ import SqlAdapter from "./src/OrganizationsBundle/adapters/sqlite/index.js";
   const adapter = await SqlAdapter(db);
   const repository = Repository(adapter);
 
-  const server = app({ repository });
+  const server = http.createServer(app({ repository }));
+  const onSignal = () => {
+    console.log("\nServer starting cleanup...");
+
+    return db.close();
+  };
+
+  const onShutdown = () => {
+    console.log("\nShutdown complete!");
+  };
+
+  const terminusOptions = {
+    signals: ["SIGINT"],
+    onSignal,
+    onShutdown,
+  };
+
+  createTerminus(server, terminusOptions);
 
   server.listen(8080, () => {
     console.log("Listening on http://localhost:8080");
